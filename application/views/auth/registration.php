6e329fdcd4c1386e74db0201584a8375dd337491
Tamara Auliani <tamaraauliani@gmail.com>
2021-10-02 23:45:46 +0800

    fixed language

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -7,7 +7,7 @@
                 <div class="col-lg">
                     <div class="p-5">
                         <div class="text-center">
-                            <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            <h1 class="h4 text-gray-900 mb-4">Belum punya akun? Daftar</h1>
                         </div>
                         <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
 
@@ -29,15 +29,15 @@
                                 <?= form_error('password1', '<small class="text-danger pl-4">', '</small'); ?>
                             </div>
                             <button type="submit" class="btn btn-primary btn-user btn-block">
-                                Register Account
+                                Registrasi Akun
                             </button>
 
                             <div class="form-group">
                                 <div class="text-center">
-                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    <a class="small" href="forgot-password.html">Lupa Password?</a>
                                 </div>
                                 <div class="text-center">
-                                    <a class="small" href="<?= base_url('auth'); ?>">Already have an account? Login!</a>
+                                    <a class="small" href="<?= base_url('auth'); ?>">Sudah memiliki akun? Masuk!</a>
                                 </div>
                             </div>
                         </form>
